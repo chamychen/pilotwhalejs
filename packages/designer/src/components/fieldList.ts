5db3d22dba65f74c777240d2a7d57208978ee538
chamy <313316785@qq.com>
2019-05-19 16:44:52 +0800

    Architecture refactoring,Update CTab

--- packages/designer/src/components/fieldList.ts
+++ packages/designer/src/components/fieldList.ts
@@ -26,5 +26,26 @@ export default ({
         this.$emit('input', newVal[0])
       }
     }
+  },
+  methods: {
+    /**
+     * 设置tab的高度
+     */
+    setTabContextHeight() {
+      let height = 0
+      let bodyHeight = window.innerHeight
+      let appBar = document.getElementById('appBar')
+      if (appBar) {
+        let appBarHeight = appBar.children[0].clientHeight
+        height = bodyHeight - appBarHeight - 48
+      } else {
+        height = bodyHeight - 48
+      }
+      this.$refs.fieldListTab.currentContextHeight = height + 'px'
+    }
+  },
+  mounted() {
+    this.setTabContextHeight()
+    window.onresize = this.setTabContextHeight
   }
 })
